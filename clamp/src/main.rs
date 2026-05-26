856854b5e400c01a43c407bff942b3228c7ecbd7
Bryce Hart <bryce.hart23@gmail.com>
2026-05-26 00:06:04 -0400

    additional comments, based off the changes I made to the original

--- clamp/src/main.rs
+++ clamp/src/main.rs
@@ -1,8 +1,10 @@
 use std::env;
 
 fn main() {
+    // Collect arguments
     let args: Vec<String> = env::args().collect();
 
+    //if its not the valid length then the expect statements will catch it below
     if args.len() < 4 {
         eprintln!("Usage: clamp <number> <min> <max>");
         std::process::exit(1);
@@ -12,11 +14,13 @@ fn main() {
     let min: f64 = args[2].parse().expect("Please provide a valid min");
     let max: f64 = args[3].parse().expect("Please provide a valid max");
 
+    //just a quick check to make sure min is less then max
     if min > max {
         eprintln!("Error: min ({}) cannot be greater than max ({})", min, max);
         std::process::exit(1);
     }
 
+    //then do the actual clamping
     if number < min {
         println!("{} is below range, clamped to min: {}", number, min);
     } else if number > max {
